fbfd8109a28a3b567228a7d2d42097d1c5eb0d5b
Florian Rappl <florian.rappl@iquestgroup.com>
2017-05-24 14:43:57 +0300

    Potential solution

--- exercises/01/src/element.jsx
+++ exercises/01/src/element.jsx
@@ -1,5 +1,43 @@
 import * as React from 'react';
 
+const spanStyle = {
+  fontFamily: 'Consolas', 
+  textDecoration: 'underline'
+};
+
 // Changes required below this line
 
-export default (/* insert JSX here */);
+const Hello = (props) => (
+<div>
+  <div>
+    <strong>
+      Hello!
+    </strong>
+    &nbsp;
+    Welcome,
+    &nbsp;
+    <span style={ spanStyle }>
+      <span style={{ color: 'green' }}>
+        { props.firstName }
+      </span>
+      &nbsp;
+      <span style={{ color: 'red' }}>
+        { props.lastName }
+      </span>
+    </span>.
+  </div>
+  {
+    (props.showCorrect) ? (
+      <div>
+        Not your name? Click <a href="#">here</a> to correct ...
+      </div>
+    ) : (false)
+  }
+</div>
+)
+
+export default (
+  <Hello firstName="Your"
+         lastName="Name"
+         showCorrect={true} />
+);
