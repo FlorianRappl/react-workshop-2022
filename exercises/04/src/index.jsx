dfe4cfa44efae9885dce5fada9f940d998d8a391
Florian Rappl <florian.rappl@iquestgroup.com>
2017-05-24 17:34:27 +0300

    Added potential solution

--- exercises/04/src/index.jsx
+++ exercises/04/src/index.jsx
@@ -1,10 +1,72 @@
 import * as React from 'react';
-import { Router, Route, Link } from 'react-router';
+import { Router, Route, Redirect, Switch } from 'react-router';
+import { Link } from 'react-router-dom';
 import { render } from 'react-dom';
+import createHashHistory from 'history/createHashHistory';
 
 // Edit below to solve the assignment
 
+import Hello from '../../02/src/component';
+import TodoList from '../../03/src/todoList';
+
+const Home = (props) => (
+  <div>
+    <h1>
+      Home
+    </h1>
+  </div>
+);
+
+const NoMatch = (props) => (
+  <div>
+    <span style={{ color: 'red' }}>
+      The link is invalid ...
+    </span>
+  </div>
+);
+
+const Menu = (props) => (
+  <ul>
+    <li>
+      <Link to="/home">
+        Home
+      </Link>
+    </li>
+    <li>
+      <Link to="/hello">
+        Hello Component
+      </Link>
+    </li>
+    <li>
+      <Link to="/todo">
+        Todo List
+      </Link>
+    </li>
+    <li>
+      <Link to="/foo">
+        Invalid link
+      </Link>
+    </li>
+  </ul>
+);
+
 render(
-  /* insert code here */,
+  <Router history={createHashHistory()}>
+    <div>
+      <Menu />
+      <Switch>
+        <Route path="/home"
+               component={Home} />
+        <Route path="/hello"
+               component={Hello} />
+        <Route path="/todo"
+               component={TodoList} />
+        <Redirect from="/"
+                  to="/home"
+                  exact />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>
+  </Router>,
   document.querySelector('#app')
 );
